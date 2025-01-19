695091e9fffbac7d1c41fbdaf2f1a2865e0d2259
Debomita-hub <debomitachakladar9@gmail.com>
2025-01-19 21:28:39 +0530

    Add files via upload

---
+++ Assignment - 1/Pro1.c
@@ -0,0 +1,12 @@
+//Write a C program to print an array.
+
+#include<stdio.h>
+int main()
+{
+	int i,s,arr[]={1,2,3,4,5};
+	s=sizeof(arr)/sizeof(arr[0]);
+	printf("The array is:\n");
+	for(i=0;i<s;i++)
+	printf("%d ",arr[i]);
+	return 0;
+}
